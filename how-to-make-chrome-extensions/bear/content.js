c3f1343e15a38d5f4ffa71e9ba5a552484e20e4f
Till <t1m4nn@gmail.com>
2020-10-12 00:22:14 +0200

    added switch case

--- how-to-make-chrome-extensions/bear/content.js
+++ how-to-make-chrome-extensions/bear/content.js
@@ -1,11 +1,20 @@
-let stateCheck = setInterval(() => {
-  if (document.readyState === "complete") {
+function clickButton(querySelector) {
+  let stateCheck = setInterval(() => {
     clearInterval(stateCheck);
     console.log("clicking ibm cloud button...");
-    document
-      .querySelector(
-        "body > main > div.ibm-cloud-app.ibm-cloud-react-container > div > div > div.login-wrapper > form.login-form > div.login-form__input-rows > div.login-form__realm-user-id-row > div.login-form__user-id-wrapper.login-form__fluid-input-label.login-form__fluid-input-label--align-top > div.login-form__login-button-wrapper > button"
-      )
-      .click();
-  }
-}, 2000);
+    document.querySelector(querySelector).click();
+  }, 2000);
+}
+
+let url = window.location.toString();
+console.log(url);
+switch (url) {
+  case "https://cloud.ibm.com/login":
+    clickButton(
+      "body > main > div.ibm-cloud-app.ibm-cloud-react-container > div > div > div.login-wrapper > form.login-form > div.login-form__input-rows > div.login-form__realm-user-id-row > div.login-form__user-id-wrapper.login-form__fluid-input-label.login-form__fluid-input-label--align-top > div.login-form__login-button-wrapper > button"
+    );
+    break;
+  case "https://w3id.sso.ibm.com/auth/sps/auth?FedName=samlidp2&FedId=uuid564ae2bf-0164-149c-8dbf-bedb46020ff9":
+    clickButton("#btn_signin");
+    break;
+}
